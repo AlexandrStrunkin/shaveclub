ef7eeac9fad36dad470bf95638d0f086b19bf827
Sergey <st@webgk.ru>
2016-05-13 09:53:42 +0300

    SUP-4280: correction scripts

--- local/templates/aspro_kshop_copy/components/bitrix/news.list/slide_home/script.js
+++ local/templates/aspro_kshop_copy/components/bitrix/news.list/slide_home/script.js
@@ -1,31 +1,4 @@
- $(".slide_news").flexslider({
-    animation: "slide",
-    selector: ".news_slider > li",
-    slideshow: false,
-    slideshowSpeed: 6000,
-    animationSpeed: 600,
-    directionNav: true,
-    animationLoop: true,
-    itemWidth: 360,
-    controlNav: true,
-    pauseOnHover: true,
-    controlsContainer: ".news_navigation_news",
-    manualControls: ".news_block.news .flex-control-nav.flex-control-paging li a"
-});
- $(".slide_sale").flexslider({
-    animation: "slide",
-    selector: ".news_slider > li",
-    slideshow: false,
-    slideshowSpeed: 6000,
-    animationSpeed: 600,
-    directionNav: true,
-    animationLoop: true,
-    itemWidth: 360,
-    controlNav: true,
-    pauseOnHover: true,
-    controlsContainer: ".news_navigation_sale",
-    manualControls: ".news_block.sale .flex-control-nav.flex-control-paging li a"
-});
+
  $(function(){
   $('body').on('click', '.slide_1', function(){
        $('#slide_1').show();
