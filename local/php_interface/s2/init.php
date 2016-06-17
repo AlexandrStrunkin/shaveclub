f6e3d9033e08cfd4c41374cf63ad382376319451
Ruslik <br@webgk.ru>
2016-06-17 12:47:32 +0300

    SUP-4543: Fixing codestyle on order making template

--- local/php_interface/s2/init.php
+++ local/php_interface/s2/init.php
@@ -1,4 +1,9 @@
 <?
+    define("PVZ_MSK_DELIVERY_FOR_CHEAP_ORDERS", 48);
+    define("PVZ_MSK_DELIVERY_FOR_EXPENSIVE_ORDERS", 47);
+    define("PVZ_SPB_DELIVERY_FOR_CHEAP_ORDERS", 50);
+    define("PVZ_SPB_DELIVERY_FOR_EXPENSIVE_ORDERS", 51);
+    
     CModule::IncludeModule("forum");
     AddEventHandler("forum", "onAfterMessageAdd", "notifyNewItemFeedback");
     function notifyNewItemFeedback($ID, $arFields) {
