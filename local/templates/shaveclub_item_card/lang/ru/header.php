7f29cc878447e91296c8a2ee731db955055da447
Sergey <st@webgk.ru>
2016-07-08 14:24:05 +0300

    SUP-4583: edit footer

--- local/templates/shaveclub_item_card/lang/ru/header.php
+++ local/templates/shaveclub_item_card/lang/ru/header.php
@@ -3,7 +3,7 @@ $MESS["SEARCH_GOODS"] = "Товары";
 $MESS["SEARCH_OTHER"] = "Прочее";
 $MESS["FOOTER_COMPANY_ABOUT"] = "Подробнее о компании";
 $MESS["FOOTER_COMPANY_PHONE"] = "Свяжитесь с нами";
-$MESS["SHAVECLUB"] = "ООО\“Шейвклаб\”";
+$MESS["SHAVECLUB"] = "ООО 'Шейвклаб'";
 $MESS["PRIVACY_POLICY"] = "Политика конфиденциальности";
 $MESS["DELIVERY"] = "Условия доставки и оплаты";
 ?>
