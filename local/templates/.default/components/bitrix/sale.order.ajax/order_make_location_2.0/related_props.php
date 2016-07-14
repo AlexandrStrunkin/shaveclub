6ad870c181ac2957ddc19d8a28cd45fcc818d29f
Sergey <st@webgk.ru>
2016-07-14 10:39:00 +0300

    SUP-4628 edit

--- local/templates/.default/components/bitrix/sale.order.ajax/order_make_location_2.0/related_props.php
+++ local/templates/.default/components/bitrix/sale.order.ajax/order_make_location_2.0/related_props.php
@@ -4,7 +4,7 @@ include($_SERVER["DOCUMENT_ROOT"].$templateFolder."/props_format.php");
 $style = (is_array($arResult["ORDER_PROP"]["RELATED"]) && count($arResult["ORDER_PROP"]["RELATED"])) ? "" : "display:none";
 ?>
 <div class="contacts-block" id="sale_order_props_excl" style="<?= $style ?>">
-    <div class="title"><?=GetMessage("SOA_TEMPL_RELATED_PROPS")?></div>
+    <div class="title"><?= GetMessage("SOA_TEMPL_RELATED_PROPS") ?></div>
     <div class="input-container">
 	    <?= PrintPropsForm($arResult["ORDER_PROP"]["RELATED"], $arParams["TEMPLATE_LOCATION"]) ?>
     </div>
