0dded1d2f6dd00f2ecfdf3e6ba4d4b892f369591
Sergey <st@webgk.ru>
2016-08-02 15:46:37 +0300

    SUP-4782: edit

--- local/templates/aspro_kshop_copy/components/bitrix/system.auth.forgotpasswd/forgot/template.php
+++ local/templates/aspro_kshop_copy/components/bitrix/system.auth.forgotpasswd/forgot/template.php
@@ -15,20 +15,20 @@ if(isset($APPLICATION->arAuthResult))
 	?>
 
 		<div class="form-block">
-			<form name="bform" method="post" target="_top" class="bf" action="<?=$arResult["AUTH_URL"]?>">
-				<?if (strlen($arResult["BACKURL"]) > 0){?><input type="hidden" name="backurl" value="<?=$arResult["BACKURL"]?>" /><?}?>
+			<form name="bform" method="post" target="_top" class="bf" action="<?= $arResult["AUTH_URL"] ?>">
+				<?if (strlen($arResult["BACKURL"]) > 0){?><input type="hidden" name="backurl" value="<?= $arResult["BACKURL"] ?>" /><? } ?>
 				<input type="hidden" name="AUTH_FORM" value="Y">
 				<input type="hidden" name="TYPE" value="SEND_PWD">
-				<?=GetMessage("AUTH_FORGOT_PASSWORD_1")?>
+				<?= GetMessage("AUTH_FORGOT_PASSWORD_1") ?>
 				<br /><br />
 				<div class="r">
-					<label><?=GetMessage("AUTH_EMAIL")?></label>
+					<label><?= GetMessage("AUTH_EMAIL") ?></label>
 					<input type="text" name="USER_LOGIN"  maxlength="255" />
 				</div>
 
 				<div class="but-r">
-					<button class="button30" type="submit" name="send_account_info" value=""><span><?=GetMessage("RETRIEVE")?></span></button>
-					<div class="prompt"><span class="star">*</span> &mdash;&nbsp; <?=GetMessage("REQUIRED_FIELDS")?></div>
+					<button class="button30" type="submit" name="send_account_info" value=""><span><?= GetMessage("RETRIEVE") ?></span></button>
+					<div class="prompt"><span class="star">*</span> &mdash;&nbsp; <?= GetMessage("REQUIRED_FIELDS") ?></div>
 					<div class="clearboth"></div>
 				</div>
 			</form>
