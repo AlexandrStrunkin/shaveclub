158bae083bf807d4e01b0b9ee698f1789b88e164
Sergey <st@webgk.ru>
2016-08-02 15:43:55 +0300

    SUP-4782: edit

--- auth/forgot-password/index.php
+++ auth/forgot-password/index.php
@@ -1,11 +1,13 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Забыли пароль");?>
 <?
-	if(!$USER->IsAuthorized()){
+	if(!$USER->IsAuthorized()) {
         $APPLICATION->IncludeComponent("bitrix:system.auth.forgotpasswd", "forgot-pass", Array(), false);
-    } elseif ( !empty( $_REQUEST["backurl"] ) ){
-        LocalRedirect( $_REQUEST["backurl"] );}
-	else{ LocalRedirect(SITE_DIR.'personal/');}
+    } elseif ( !empty( $_REQUEST["backurl"] ) ) {
+        LocalRedirect( $_REQUEST["backurl"] );
+    } else {
+        LocalRedirect(SITE_DIR.'personal/');
+    }
 ?>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
