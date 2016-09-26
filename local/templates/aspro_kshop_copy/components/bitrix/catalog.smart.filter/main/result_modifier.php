5f77495df809f5523d0be80fb1c883ab749b4779
Sergey <st@webgk.ru>
2016-09-26 12:49:32 +0300

    SUP-5062: edit section and product

--- local/templates/aspro_kshop_copy/components/bitrix/catalog.smart.filter/main/result_modifier.php
+++ local/templates/aspro_kshop_copy/components/bitrix/catalog.smart.filter/main/result_modifier.php
@@ -9,12 +9,21 @@ $arResult["SPECIALS_BLOCK"]["HTML"] = "";
 $arResult["SPECIALS_BLOCK"]["OPENED"] = null;
 foreach($arResult["ITEMS"] as $key => $arItem)
 {
+    if($arItem["CODE"] == "COMPATIBILITY"){     // перезапись неактивных элементов в свойстве "совместимость"
+        foreach($arItem["VALUES"] as $k => $ar){
+            if(!empty($ar["VALUE"])){
+                $item_property[$k] = $ar;
+            }
+        }
+
+    $arResult["ITEMS"][$key]["VALUES"] = $item_property;  // перезапись неактивных элементов в свойстве "совместимость"
+    }
 	if( $arItem["CODE"] == "HIT")
 	{
 		foreach($arItem["VALUES"] as $k => $ar){
-			$arResult["SPECIALS_BLOCK"]["HTML"] .= 
+			$arResult["SPECIALS_BLOCK"]["HTML"] .=
 				'<div class="specials_'.strtolower($arHitPropertyValues[$k]["XML_ID"]).($ar["DISABLED"] ? ' disabled': '').'">
-					<input type="checkbox" value="'.$ar["HTML_VALUE"].'" name="'.$ar["CONTROL_NAME"].'" 
+					<input type="checkbox" value="'.$ar["HTML_VALUE"].'" name="'.$ar["CONTROL_NAME"].'"
 				id="'.$ar["CONTROL_ID"].'"'.($ar["CHECKED"]?'checked="checked"':'').' onclick="smartFilter.click(this)"'.($ar["DISABLED"]?' disabled':'').' />
 					<label for="'.$ar["CONTROL_ID"].'"><i class="icon"></i><span>'.$ar["VALUE"].'</span></label>
 				</div>';
--- local/templates/aspro_kshop_copy/components/bitrix/catalog.smart.filter/main/template.php
+++ local/templates/aspro_kshop_copy/components/bitrix/catalog.smart.filter/main/template.php
@@ -1,29 +1,29 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?
 	CJSCore::Init(array("fx"));
-		
+
 	if (!empty($_COOKIE["KSHOP_FILTER_CLOSED"]))
 	{
-		$arCookies =  json_decode($_COOKIE["KSHOP_FILTER_CLOSED"]);	
-		array_unique ($arCookies);		
-		unset ($_COOKIE["KSHOP_FILTER_CLOSED"]);		
-		setcookie ($_COOKIE["KSHOP_FILTER_CLOSED"], null, -1);			
+		$arCookies =  json_decode($_COOKIE["KSHOP_FILTER_CLOSED"]);
+		array_unique ($arCookies);
+		unset ($_COOKIE["KSHOP_FILTER_CLOSED"]);
+		setcookie ($_COOKIE["KSHOP_FILTER_CLOSED"], null, -1);
 		foreach($arCookies as $key => $value)
-		{ foreach($arResult["ITEMS"] as $key=>$property) { if ($property["ID"]==$value) { $arResult["ITEMS"][$key]["OPENED"]="N"; } } }	
+		{ foreach($arResult["ITEMS"] as $key=>$property) { if ($property["ID"]==$value) { $arResult["ITEMS"][$key]["OPENED"]="N"; } } }
 		if (in_array("specials", $arCookies)){ $arResult["SPECIALS_BLOCK"]["OPENED"]="N"; }
 	}
 	if (!empty($_COOKIE["KSHOP_FILTER_OPENED"]))
 	{
-		$arCookies =  json_decode($_COOKIE["KSHOP_FILTER_OPENED"]);	
-		array_unique ($arCookies);		
-		unset ($_COOKIE["KSHOP_FILTER_OPENED"]);		
-		setcookie ($_COOKIE["KSHOP_FILTER_OPENED"], null, -1);		
+		$arCookies =  json_decode($_COOKIE["KSHOP_FILTER_OPENED"]);
+		array_unique ($arCookies);
+		unset ($_COOKIE["KSHOP_FILTER_OPENED"]);
+		setcookie ($_COOKIE["KSHOP_FILTER_OPENED"], null, -1);
 		foreach($arCookies as $key => $value)
-		{ foreach($arResult["ITEMS"] as $key=>$property) { if ($property["ID"]==$value) { $arResult["ITEMS"][$key]["OPENED"]="Y"; } } }	
-		if (in_array("specials", $arCookies)){ $arResult["SPECIALS_BLOCK"]["OPENED"]="Y"; }		
+		{ foreach($arResult["ITEMS"] as $key=>$property) { if ($property["ID"]==$value) { $arResult["ITEMS"][$key]["OPENED"]="Y"; } } }
+		if (in_array("specials", $arCookies)){ $arResult["SPECIALS_BLOCK"]["OPENED"]="Y"; }
 	}
 	$propsCount = 0;
-	foreach($arResult["ITEMS"] as $key=>$arItem) 
+	foreach($arResult["ITEMS"] as $key=>$arItem)
 	{
 		if((isset($arItem["PRICE"]) && !(!$arItem["VALUES"]["MIN"]["VALUE"] || !$arItem["VALUES"]["MAX"]["VALUE"] || $arItem["VALUES"]["MIN"]["VALUE"] == $arItem["VALUES"]["MAX"]["VALUE"])) || (!isset($arItem["PRICE"])&&( $arItem["PROPERTY_TYPE"] == "N" && !empty($arItem["VALUES"]["MIN"]["VALUE"]) || $arItem["PROPERTY_TYPE"] != "N" && !empty($arItem["VALUES"]) )&& (!($arItem["PROPERTY_TYPE"]=="N"&&($arItem["VALUES"]["MIN"]["VALUE"]==$arItem["VALUES"]["MAX"]["VALUE"]))))) $propsCount++;
 	}
@@ -33,17 +33,17 @@
 		<div class="bx_filter_section m4">
 			<form name="<?echo $arResult["FILTER_NAME"]."_form"?>" action="<?echo $arResult["FORM_ACTION"]?>" method="get" class="smartfilter">
 				<?foreach($arResult["HIDDEN"] as $arItem):?><input type="hidden" name="<?echo $arItem["CONTROL_NAME"]?>" id="<?echo $arItem["CONTROL_ID"]?>" value="<?echo $arItem["HTML_VALUE"]?>" /><?endforeach;?>
-				
+
 				<?$iOpened=0;$firstTitle=true;?>
 				<?if (strlen($arResult["SPECIALS_BLOCK"]["HTML"])):?>
 					<?if ($firstTitle){$firstTitle = false;} $iOpened++;?>
-					<div class="bx_filter_container<?if($arResult["SPECIALS_BLOCK"]["OPENED"]=="Y" || ($iOpened<=3 && $arResult["SPECIALS_BLOCK"]["OPENED"]!="N")):?> active<?endif;?>" property_id="specials">						
+					<div class="bx_filter_container<?if($arResult["SPECIALS_BLOCK"]["OPENED"]=="Y" || ($iOpened<=3 && $arResult["SPECIALS_BLOCK"]["OPENED"]!="N")):?> active<?endif;?>" property_id="specials">
 						<div class="bx_filter_container_title no_border"><span class="name"><span><?=GetMessage("SPECIAL_BLOCK_TITLE")?></span></span><i class="arr"></i></div>
 						<div class="bx_filter_block<?if($arResult["SPECIALS_BLOCK"]["OPENED"]=="Y" || ($iOpened<=3 && $arResult["SPECIALS_BLOCK"]["OPENED"]!="N")):?> active<?endif;?>"><?=$arResult["SPECIALS_BLOCK"]["HTML"]?></div>
 						<span class="bx_filter_container_modef"></span>
 					</div>
-				<?endif;?>			
-				
+				<?endif;?>
+
 				<?foreach($arResult["ITEMS"] as $key=>$arItem):?>
 					<?if(isset($arItem["PRICE"])){?>
 						<?
@@ -54,7 +54,7 @@
 							if (!$cur_max) $cur_max = 50000;
 							$iOpened++;
 						?>
-						<div class="bx_filter_container price<?if($arItem["OPENED"]=="Y" || ($iOpened<=3 && $arItem["OPENED"]!="N")):?> active<?endif;?>" property_id="<?=$arItem["ID"]?>">	
+						<div class="bx_filter_container price<?if($arItem["OPENED"]=="Y" || ($iOpened<=3 && $arItem["OPENED"]!="N")):?> active<?endif;?>" property_id="<?=$arItem["ID"]?>">
 							<div class="bx_filter_container_title<?if($firstTitle):?> no_border<?endif;?>"><span class="name"><span><?=$arItem["NAME"]?></span></span><i class="arr"></i></div>
 							<?if ($firstTitle){$firstTitle = false;}?>
 							<div class="bx_filter_block<?if($arItem["OPENED"]=="Y" || ($iOpened<=3 && $arItem["OPENED"]!="N")):?> active<?endif;?>">
@@ -65,7 +65,7 @@
 										class="min-price"
 										type="text"
 										name="<?echo $arItem["VALUES"]["MIN"]["CONTROL_NAME"]?>"
-										id="<?echo $arItem["VALUES"]["MIN"]["CONTROL_ID"]?>" 
+										id="<?echo $arItem["VALUES"]["MIN"]["CONTROL_ID"]?>"
 										value="<?echo $arItem["VALUES"]["MIN"]["HTML_VALUE"]?>"
 										placeholder="<?=floor($cur_min);?>"
 										size="5"
@@ -105,8 +105,8 @@
 						</script>
 					<?}?>
 				<?endforeach?>
-				
-				<?$i=0;?>		
+
+				<?$i=0;?>
 				<?foreach($arResult["ITEMS"] as $arItem){
 					if(!isset($arItem["PRICE"])&&( $arItem["PROPERTY_TYPE"] == "N" && !empty($arItem["VALUES"]["MIN"]["VALUE"]) || $arItem["PROPERTY_TYPE"] != "N" && !empty($arItem["VALUES"]) )&&
 					(!($arItem["PROPERTY_TYPE"]=="N"&&($arItem["VALUES"]["MIN"]["VALUE"]==$arItem["VALUES"]["MAX"]["VALUE"])))){?>
@@ -119,7 +119,7 @@
 								$cur_max = !empty($arItem["VALUES"]["MAX"]["HTML_VALUE"]) ? ceil($arItem["VALUES"]["MAX"]["HTML_VALUE"]) : ceil($arItem["VALUES"]["MAX"]["VALUE"]);
 								if($arParams["SHOW_HINTS"]=="Y") { $prop = CIBlockProperty::GetByID($arItem["ID"], $arParams["IBLOCK_ID"])->GetNext(); }?>
 								<?$iOpened++;?>
-									
+
 									<div class="bx_filter_container<?if($arItem["OPENED"]=="Y" || ($iOpened<=3 && $arItem["OPENED"]!="N")):?> active<?endif;?>" property_id="<?=$arItem["ID"]?>">
 										<div class="bx_filter_container_title"><span class="name"><span><?=$arItem["NAME"]?></span></span><?if ($prop["HINT"]):?><span class="hint"><span class="hint_icon"><i>?</i></span><b class="triangle"></b><div class="tooltip"><a class="tooltip_close">&times;</a><?=$prop["HINT"]?></div></span><?endif;?><i class="arr"></i></div>
 										<div class="bx_filter_block<?if($arItem["OPENED"]=="Y" || ($iOpened<=3 && $arItem["OPENED"]!="N")):?> active<?endif;?>">
@@ -131,7 +131,7 @@
 													class="min-input"
 													type="text"
 													name="<?echo $arItem["VALUES"]["MIN"]["CONTROL_NAME"]?>"
-													id="<?echo $arItem["VALUES"]["MIN"]["CONTROL_ID"]?>" 
+													id="<?echo $arItem["VALUES"]["MIN"]["CONTROL_ID"]?>"
 													value="<?echo $arItem["VALUES"]["MIN"]["HTML_VALUE"]?>"
 													placeholder="<?=floor($cur_min);?>"
 													size="5"
@@ -142,7 +142,7 @@
 													class="max-input"
 													type="text"
 													name="<?echo $arItem["VALUES"]["MAX"]["CONTROL_NAME"]?>"
-													id="<?echo $arItem["VALUES"]["MAX"]["CONTROL_ID"]?>" 
+													id="<?echo $arItem["VALUES"]["MAX"]["CONTROL_ID"]?>"
 													value="<?echo $arItem["VALUES"]["MAX"]["HTML_VALUE"]?>"
 													placeholder="<?=ceil($cur_max);?>"
 													size="5"
@@ -157,7 +157,7 @@
 										</div>
 										<span class="bx_filter_container_modef"></span>
 									</div>
-									
+
 									<script type="text/javascript">
 										var DoubleTrackBar<?=$arItem["VALUES"]["MAX"]["CONTROL_ID"]?> = new cDoubleTrackBar('drag_track_<?=$arItem["VALUES"]["MAX"]["CONTROL_ID"]?>', 'drag_tracker_<?=$arItem["VALUES"]["MAX"]["CONTROL_ID"]?>', 'left_slider_<?=$arItem["VALUES"]["MAX"]["CONTROL_ID"]?>', 'right_slider_<?=$arItem["VALUES"]["MAX"]["CONTROL_ID"]?>', {
 											Min: parseFloat(<?=floor($arItem["VALUES"]["MIN"]["VALUE"]);?>),
@@ -169,20 +169,23 @@
 											RoundTo: 1
 										});
 									</script>
-							
+
 								<?} elseif(!empty($arItem["VALUES"]) && !isset($arItem["PRICE"])){?>
 									<?if($arItem["CODE"] == "HIT"){continue;}?>
 									<? if($arParams["SHOW_HINTS"]=="Y") { $prop = CIBlockProperty::GetByID($arItem["ID"], $arParams["IBLOCK_ID"])->GetNext(); } ?>
-									
+
 									<?foreach($arItem["VALUES"] as $val => $ar):?>
 										<? if ($ar["CHECKED"]) { $arItem["OPENED"]="Y"; } ?>
 									<?endforeach;?>
-									
+
 									<?$iOpened++;?>
+
 									<div class="bx_filter_container<?if($arItem["OPENED"]=="Y" || ($iOpened<=3 && $arItem["OPENED"]!="N")):?> active<?endif;?>" property_id="<?=$arItem["ID"]?>">
 										<div class="bx_filter_container_title"><span class="name"><span><?=$arItem["NAME"]?></span></span><?if ($prop["HINT"]):?><span class="hint"><span class="hint_icon"><i>?</i></span><b class="triangle"></b><div class="tooltip"><a class="tooltip_close">&times;</a><?=$prop["HINT"]?></div></span><?endif;?><i class="arr"></i></div>
 										<div class="bx_filter_block<?if($arItem["OPENED"]=="Y" || ($iOpened<=3 && $arItem["OPENED"]!="N")):?> active<?endif;?><?if(count($arItem["VALUES"])>6):?> scrollable<?endif;?>">
-											<?foreach($arItem["VALUES"] as $val => $ar):?>
+
+                                            <?foreach($arItem["VALUES"] as $val => $ar):?>
+
 												<div class="<?echo $ar["DISABLED"] ? 'disabled': ''?>">
 													<input
 														type="checkbox"
@@ -193,6 +196,7 @@
 														onclick="smartFilter.click(this)"
 														<?if ($ar["DISABLED"]):?>disabled<?endif?>
 													/>
+
 													<label for="<?echo $ar["CONTROL_ID"]?>"><?echo $ar["VALUE"];?></label>
 												</div>
 											<?endforeach;?>
@@ -202,7 +206,7 @@
 								<?}?>
 					<?}?>
 				<?}?>
-						
+
 				<div style="clear: both;"></div>
 				<div class="bx_filter_control_section">
 					<div class="for_button">
@@ -242,7 +246,7 @@
 					item = $(item);
 					var propID = item.parents(".bx_filter_container").attr("property_id");
 					if (!item.parents(".bx_filter_container").is(".active")) { if (!$.inArray(propID, arClosed) >= 0) { arClosed.push(propID); } }
-						else { if ($.inArray(propID, arClosed) >= 0) { arClosed.splice(delIndex,1); } } 
+						else { if ($.inArray(propID, arClosed) >= 0) { arClosed.splice(delIndex,1); } }
 				}
 			}
 			$.cookie("KSHOP_FILTER_CLOSED", arClosed);
@@ -251,7 +255,7 @@
 
 		var checkOpened = function(item)
 		{
-			
+
 			$.cookie.json = true;
 			var arOpened = $.cookie("KSHOP_FILTER_OPENED");
 			if (arOpened && typeof arOpened != "undefined")
@@ -260,17 +264,17 @@
 				{
 					var propID = item.parents(".bx_filter_container").attr("property_id");
 					var delIndex = $.inArray(propID, arOpened);
-					if (delIndex >= 0) { arOpened.splice(delIndex,1); checkClosed(item); } 
+					if (delIndex >= 0) { arOpened.splice(delIndex,1); checkClosed(item); }
 						else { arOpened.push(propID); checkClosed(item); }
 				}
 				else
 				{
-					$(".bx_filter_container").each(function() 
-					{ 
-						var propID = $(this).attr("property_id");	
-						if ($(this).is(".active")) { if ($.inArray(propID, arOpened) < 0) { arOpened.push(propID); checkClosed(item); } } 
+					$(".bx_filter_container").each(function()
+					{
+						var propID = $(this).attr("property_id");
+						if ($(this).is(".active")) { if ($.inArray(propID, arOpened) < 0) { arOpened.push(propID); checkClosed(item); } }
 					});
-				}	
+				}
 			}
 			else
 			{
@@ -280,14 +284,14 @@
 					item = $(item);
 					var propID = item.parents(".bx_filter_container").attr("property_id");
 					if (item.parents(".bx_filter_container").is(".active")) { if (!$.inArray(propID, arOpened) >= 0) { arOpened.push(propID); checkClosed(item); }  }
-						else { if ($.inArray(propID, arOpened) >= 0) { arOpened.splice(delIndex,1); checkClosed(item); } } 	
+						else { if ($.inArray(propID, arOpened) >= 0) { arOpened.splice(delIndex,1); checkClosed(item); } }
 				}
 				else
 				{
-					$(".bx_filter_container").each(function() 
-					{ 
+					$(".bx_filter_container").each(function()
+					{
 						var propID = $(this).attr("property_id");
-						if ($(this).is(".active")) { if ($.inArray(propID, arOpened) < 0) { arOpened.push(propID); checkClosed(item); } } 
+						if ($(this).is(".active")) { if ($.inArray(propID, arOpened) < 0) { arOpened.push(propID); checkClosed(item); } }
 					});
 				}
 			}
@@ -295,9 +299,9 @@
 			return true;
 		}
 		checkOpened();
-		
+
 		var smartFilter = new JCSmartFilter('<?echo CUtil::JSEscape($arResult["FORM_ACTION"])?>');
-			
+
 		$(".bx_filter_container_title").click( function()
 		{
 			if ($(this).parents(".bx_filter_container").hasClass("active")) { $(this).next(".bx_filter_block").slideUp(100); }
@@ -305,22 +309,22 @@
 			$(this).parents(".bx_filter_container").toggleClass("active");
 			checkOpened($(this));
 		});
-		
-		
+
+
 		$(".hint .hint_icon").click(function(e)
 		{
 			var tooltipWrapp = $(this).parents(".hint");
 			tooltipWrapp.click(function(e){e.stopPropagation();})
 			if (tooltipWrapp.is(".active"))
 			{
-				tooltipWrapp.removeClass("active").find(".tooltip").slideUp(200); 
+				tooltipWrapp.removeClass("active").find(".tooltip").slideUp(200);
 			}
 			else
 			{
 				tooltipWrapp.addClass("active").find(".tooltip").slideDown(200);
-				tooltipWrapp.find(".tooltip_close").click(function(e) { e.stopPropagation(); tooltipWrapp.removeClass("active").find(".tooltip").slideUp(100);});	
-				$(document).click(function() { tooltipWrapp.removeClass("active").find(".tooltip").slideUp(100);});				
+				tooltipWrapp.find(".tooltip_close").click(function(e) { e.stopPropagation(); tooltipWrapp.removeClass("active").find(".tooltip").slideUp(100);});
+				$(document).click(function() { tooltipWrapp.removeClass("active").find(".tooltip").slideUp(100);});
 			}
-		});	
+		});
 	</script>
 <?}?>
